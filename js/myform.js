602d1e21fb4d95a3206e0194d26eb9f5fe667b43
rachelsimms <62067024+rachelsimms@users.noreply.github.com>
2020-10-06 13:30:22 -0400

    Initial commit

---
+++ js/myform.js
@@ -0,0 +1,25 @@
+// JavaScript Document
+
+//By: Rachel Simms (Broadbent)
+
+// if user enters input area - background turn light blue
+var input = document.getElementByTagName(“input”);
+input.addEventListener(“focus”, chgColor());
+
+function chgColor(){
+    input.style.backgroundColor = “#000000”;
+}
+//if user enters into textarea - background turn light blue
+var message = document.getElementById(“message);
+
+
+
+// if user has empty fields - background turn red
+
+function subForm (e) {
+    if (input.value === null || message.value === null) {
+	        e.target.style.backgroundColor = "red";
+    } else {
+        alert('Thank you for submitting your form.')
+    }
+}
